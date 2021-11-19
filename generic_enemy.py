a50ba8873179e6cf5bafe71054adca0e90696f91
Ahmed Mahmoud Azzam <gamerazzam@gmail.com>
2021-11-19 10:29:42 +0200

    +added range to GenericEnemy

--- generic_enemy.py
+++ generic_enemy.py
@@ -11,15 +11,28 @@ idle = [pygame.image.load('Textures/frames/knight_m_idle_anim_f0.png'), pygame.i
 
 
 class GenericEnemy(GenericEntity):
-    def __init__(self, player, x=700, y=700, running=run, idiling=idle, sounds=sound, scale=(50, 80), speed=2):
+    def __init__(self, player, x=700, y=700, running=run, idiling=idle, sounds=sound, scale=(50, 80), speed=2, range=100):
         super().__init__(x, y, running, idiling, sounds, scale, speed)
         self.player = player
         self.dx = 0
         self.dy = 0
+        self.range = range
+
+    def isInRange(self):
+        distance = math.sqrt((self.player.x-self.x)**2 +
+                             (self.player.y-self.y)**2)
+        if distance <= self.range:
+            return True
+        else:
+            return False
 
     def move(self):
-        dx, dy = self.player.x-self.x, self.player.y-self.y
-        dist = math.hypot(dx, dy)
-        dx, dy = dx/dist, dy/dist
-        self.velX = dx*self.speed
-        self.velY = dy*self.speed
+        if self.isInRange():
+            dx, dy = self.player.x-self.x, self.player.y-self.y
+            dist = math.hypot(dx, dy)
+            dx, dy = dx/dist, dy/dist
+            self.velX = dx*self.speed
+            self.velY = dy*self.speed
+        else:
+            self.velX = 0
+            self.velY = 0
