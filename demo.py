51ecc91292b1887608e4d05a78dccb9cad1e0e40
ibrahemyasser <ebrahim195016@feng.bu.edu.eg>
2021-12-14 19:06:08 +0200

    added a basic ui class to the project

--- demo.py
+++ demo.py
@@ -3,6 +3,7 @@ from player import Player
 from generic_enemy import GenericEnemy
 from sys import exit
 from weapon import Weapon
+from ui import Ui,Start
 # Starts & intiates pygame
 pygame.init()
 weaponO = Weapon()
@@ -14,14 +15,17 @@ player = pygame.sprite.Group()
 player.add(playerO)
 WIDTH = 1366
 HEIGHT = 768
+started = True
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption('Soup')
 # You can also change the icon
 clock = pygame.time.Clock()
 floor_surface = pygame.image.load('Textures/frames/floor_1.png').convert()
-
+ui0 = Ui(screen,WIDTH,HEIGHT,floor_surface,started)
+start = Start(screen,WIDTH,HEIGHT,floor_surface,started)
 
 while True:
+    start.startUi()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
@@ -55,6 +59,8 @@ while True:
             weaponO.attackDelay()
         if event.type == pygame.MOUSEBUTTONUP:
             weaponO.attackFlag = False
+        if event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                ui0.paused()
     # draw all out elements
     # Updates the display
     for i in range(0, HEIGHT, 16):
